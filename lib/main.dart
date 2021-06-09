d64751d54f525c45da218cee3d92a60e791944c7
Anaïs Betts <anais@anaisbetts.org>
2021-06-10 00:37:34 +0200

    Don't play audio when the app is backgrounded

--- lib/main.dart
+++ lib/main.dart
@@ -28,17 +28,38 @@ class Poolsuite extends StatefulWidget {
   _PoolsuiteState createState() => _PoolsuiteState();
 }
 
-class _PoolsuiteState extends State<Poolsuite> {
+class _PoolsuiteState extends State<Poolsuite> with WidgetsBindingObserver {
+  AppLifecycleState _lastLifecycleState = AppLifecycleState.resumed;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    WidgetsBinding.instance!.addObserver(this);
+    WebView.platform = SurfaceAndroidWebView();
+  }
 
-    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  @override
+  void dispose() {
+    WidgetsBinding.instance!.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    setState(() {
+      _lastLifecycleState = state;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    // NB: If you background the app, the HTML5 audio keeps playing but doesn't
+    // get enough CPU time and it starts dropping out / sounding absolutely
+    // horrendous
+    if (_lastLifecycleState != AppLifecycleState.resumed) {
+      return Container();
+    }
+
     return Flex(
         direction: Axis.vertical,
         mainAxisAlignment: MainAxisAlignment.start,
@@ -52,11 +73,19 @@ class _PoolsuiteState extends State<Poolsuite> {
             onWebViewCreated: (WebViewController controller) async {
               while (true) {
                 await Future.delayed(Duration(seconds: 2));
+                if (_lastLifecycleState != AppLifecycleState.resumed) {
+                  return;
+                }
+
                 final isLoaded = await controller.evaluateJavascript(
                     "!!document.querySelector('.is-launcher li')");
 
                 log("checking for loaded!");
                 if (isLoaded == 'true') break;
+              }
+
+              if (_lastLifecycleState != AppLifecycleState.resumed) {
+                return;
               }
 
               await controller.evaluateJavascript(
